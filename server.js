dde069362445565901a2a742b67469446ac63d6c
KChino <kevintlapizaga@gmail.com>
2023-02-09 16:13:09 -0300

    change main index.js to server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const mysql = require("mysql2/promise");
+const router = require("./routes");
+const dbInitialSetup = require("./dbInitialSetup");
+
+app.set("view engine", "ejs");
+
+app.use(router);
+
+dbInitialSetup();
+
+app.listen(port, () => {
+  console.log("Escuchando en puerto 3000");
+});
