1e9805716d31dc510c74523151e99e0d1889b6e2
KChino <kevintlapizaga@gmail.com>
2023-02-10 17:39:32 -0300

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/articleController");
+const userController = require("../controllers/userController");
 
 // Rutas relacionadas a los usuarios:
 // ...
