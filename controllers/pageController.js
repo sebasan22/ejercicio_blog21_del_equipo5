cc7eec634c32a067553e49639f999b086110f68e
KChino <kevintlapizaga@gmail.com>
2023-02-10 12:05:31 -0300

    Update pageController.js

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -8,7 +8,7 @@ async function showHome(req, res) {
 async function showArticulos(req, res) {
   const article = await Article.findByPk(req.params.id, { include: User });
   console.log(article);
-  //res.render("articulos", { article });
+  res.render("articulos", { article });
 }
 
 async function showAdmin(req, res) {
