2858142b0774d381a1aecde5a9f19892a3adc14d
KChino <kevintlapizaga@gmail.com>
2023-02-10 11:23:05 -0300

    Update publicRoutes.js

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const pageController = require("../controllers/pageController");
 
 router.get("/", pageController.showHome);
+router.get("/articulos/:id", pageController.showArticulos);
 
 module.exports = router;
