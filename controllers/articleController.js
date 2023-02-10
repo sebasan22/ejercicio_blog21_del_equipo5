93447462a302d732a3d44282831541a1c8b26886
Emi <emi.lng711@gmail.com>
2023-02-10 15:09:50 -0300

    cambios

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -7,7 +7,9 @@ async function index(req, res) {}
 async function show(req, res) {}
 
 // Show the form for creating a new resource
-async function create(req, res) {}
+async function create(req, res) {
+  res.render("new");
+}
 
 // Store a newly created resource in storage.
 async function store(req, res) {}
--- routes/privateRoutes.js
+++ routes/privateRoutes.js
@@ -5,5 +5,5 @@ const articleController = require("../controllers/articleController");
 
 router.get("/admin", pageController.showAdmin);
 router.get("/admin/eliminar/:id", articleController.destroy);
-
+router.get("/admin/new", articleController.create);
 module.exports = router;
