9bad812bbcaf8ef81d519fc110fb6dd2b04f8855
Sebasan22 <callofsebas@hotmail.com>
2023-02-11 16:29:48 -0300

    dirección de edit:id

--- routes/privateRoutes.js
+++ routes/privateRoutes.js
@@ -6,5 +6,5 @@ const articleController = require("../controllers/articleController");
 router.get("/admin", pageController.showAdmin);
 router.get("/admin/eliminar/:id", articleController.destroy);
 router.get("/admin/new", articleController.create);
-router.get("/admin/edit", articleController.edit);
+router.get("/admin/edit/:id", articleController.edit);
 module.exports = router;
