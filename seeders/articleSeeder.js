9a3bad2ef27a5942b1587526ccbde793cd3d84db
KChino <kevintlapizaga@gmail.com>
2023-02-13 11:44:01 -0300

    Add comments

--- seeders/articleSeeder.js
+++ seeders/articleSeeder.js
@@ -1,11 +1,12 @@
 const { faker } = require("@faker-js/faker");
-const { Article, User } = require("../models");
+const { Article, User, Comment } = require("../models");
 
 faker.locale = "es";
 
 module.exports = async () => {
   const articles = [];
   const users = [];
+  const comments = [];
   for (let i = 0; i < 15; i++) {
     let firstname = faker.name.firstName();
     let lastname = faker.name.lastName();
@@ -19,9 +20,15 @@ module.exports = async () => {
       img: faker.image.nature(480, 480, true),
       userId: Math.floor(Math.random() * 14) + 1,
     });
+    comments.push({
+      content: faker.lorem.paragraphs(),
+      userId: Math.floor(Math.random() * 14) + 1,
+      articleId: Math.floor(Math.random() * 39) + 1,
+    });
   }
   await User.bulkCreate(users);
   await Article.bulkCreate(articles);
+  await Comment.bulkCreate(comments);
 
   console.log("[Database] Se corrió el seeder de Articles.");
 };
