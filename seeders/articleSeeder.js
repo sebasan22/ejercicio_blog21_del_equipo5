5541f37a82d67c667ca22dfbcfd3a236279c980e
magguer <martintelaguer@gmail.com>
2023-02-15 08:50:15 -0300

    seeders-refact

--- seeders/articleSeeder.js
+++ seeders/articleSeeder.js
@@ -9,12 +9,11 @@ module.exports = async () => {
   const articles = [];
   const comments = [];
   for (let i = 0; i < 5; i++) {
-    let username = faker.internet.userName();
-    let email = faker.internet.email();
-    let password = await bcrypt.hash("1234", 8);
-    users.push({ username, email, password });
-  }
-  for (let i = 0; i < 5; i++) {
+    users.push({
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      password: await bcrypt.hash("1234", 8)
+    })
     articles.push({
       title: faker.lorem.sentence(5),
       content: faker.lorem.paragraphs(5),
@@ -27,9 +26,9 @@ module.exports = async () => {
       articleId: Math.floor(Math.random() * 4) + 1,
     });
   }
-  await User.bulkCreate(users);
-  await Article.bulkCreate(articles);
-  await Comment.bulkCreate(comments);
+  /*   await User.bulkCreate(users);
+    await Article.bulkCreate(articles);
+    await Comment.bulkCreate(comments); */
 
   console.log("[Database] Se corrió el seeder de Articles.");
 };
