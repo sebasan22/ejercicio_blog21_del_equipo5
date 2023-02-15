3fc31c5fe0842efcff0adfb812b8a338ca543b19
magguer <martintelaguer@gmail.com>
2023-02-15 16:49:34 -0300

    articles-fix-commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 
-router.get("/logout", authController.login)
+router.get("/logout", authController.logout)
 
 module.exports = router;
